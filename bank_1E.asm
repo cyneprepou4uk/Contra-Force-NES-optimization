4ed119bc003870296686e67feac5c195b1ce08a3
Cyneprepou4uk <cyneprepou4uk@gmail.com>
2025-06-18 22:52:45 +0300

    перемещение DPCM в начало банка
    в таблице уменьшены на 01 значения для 4012

--- bank_1E.asm
+++ bank_1E.asm
@@ -64,8 +64,7 @@
 
 
 
-; bzk garbage, unused bank id
-- D 2 - - - 0x01C010 07:C000: 1E        .byte con_prg_bank + $0E + $10   ; 
+.incbin "DPCM.bin"
 
 
 
@@ -75,20 +74,14 @@ tbl_0x01C011_for_4010_4013:
 ;                                              |    |    +---------- 4012
 ;                                              |    |    |    +----- 4013
 ;                                              |    |    |    |
-- D 2 - - - 0x01C011 07:C001: 0F        .byte $0F, $00, $01, $4C   ; AC 
-- D 2 - - - 0x01C015 07:C005: 0F        .byte $0F, $00, $14, $4C   ; AD 
-- D 2 - - - 0x01C019 07:C009: 0F        .byte $0F, $3F, $27, $28   ; AE 
-- D 2 - - - 0x01C01D 07:C00D: 0F        .byte $0F, $00, $27, $28   ; AF 
-- D 2 - - - 0x01C021 07:C011: 0D        .byte $0D, $4F, $31, $24   ; B0 
-- D 2 - - - 0x01C025 07:C015: 0E        .byte $0E, $4F, $31, $24   ; B1 
-- D 2 - - - 0x01C029 07:C019: 0F        .byte $0F, $4F, $31, $24   ; B2 
-- D 2 - - - 0x01C02D 07:C01D: 0F        .byte $0F, $3F, $3A, $08   ; B3 
-
-
-
-.segment "DPCM"
-.org $C040  ; for listing file
-.incbin "DPCM.bin"
+- D 2 - - - 0x01C011 07:C001: 0F        .byte $0F, $00, $00, $4C   ; AC 
+- D 2 - - - 0x01C015 07:C005: 0F        .byte $0F, $00, $13, $4C   ; AD 
+- D 2 - - - 0x01C019 07:C009: 0F        .byte $0F, $3F, $26, $28   ; AE 
+- D 2 - - - 0x01C01D 07:C00D: 0F        .byte $0F, $00, $26, $28   ; AF 
+- D 2 - - - 0x01C021 07:C011: 0D        .byte $0D, $4F, $30, $24   ; B0 
+- D 2 - - - 0x01C025 07:C015: 0E        .byte $0E, $4F, $30, $24   ; B1 
+- D 2 - - - 0x01C029 07:C019: 0F        .byte $0F, $4F, $30, $24   ; B2 
+- D 2 - - - 0x01C02D 07:C01D: 0F        .byte $0F, $3F, $39, $08   ; B3 
 
 
 ; bzk warning, CF00 byte is a part of DPCM, but not included in binary
