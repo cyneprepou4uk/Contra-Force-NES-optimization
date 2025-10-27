b6719f86c5e26d9ac7a1cf1bdc2428a153790ee1
Cyneprepou4uk <cyneprepou4uk@gmail.com>
2025-10-28 00:03:20 +0300

    перемещение кода

--- bank_04.asm
+++ bank_04.asm
@@ -608,7 +608,7 @@ tbl_8252:
 - - - - - - 0x008263 02:8253: 06        .byte con_prg_pair + $00   ; 02 unused?
 - D 0 - - - 0x008264 02:8254: 06        .byte con_prg_pair + $10   ; 03 
 - D 0 - - - 0x008265 02:8255: 06        .byte con_prg_pair + $10   ; 04 
-- D 0 - - - 0x008266 02:8256: 02        .byte con_prg_pair + $02   ; 05 
+- D 0 - - - 0x008266 02:8256: 02        .byte con_prg_pair + $10   ; 05 
 - D 0 - - - 0x008267 02:8257: 06        .byte con_prg_pair + $06   ; 06 
 - D 0 - - - 0x008268 02:8258: 06        .byte con_prg_pair + $06   ; 07 
 - D 0 - - - 0x008269 02:8259: 02        .byte con_prg_pair + $02   ; 08 
